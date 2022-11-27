1004287f4aefe1a540c1d83af65f049a0a647180
unknown <perviz.elizade.2002@mail.ru>
2022-11-27 23:02:39 +0400

    first commit

---
+++ ConsoleApp16/Extension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp16
+{
+    static class Extension
+    {
+        public static bool CustomContain(this string sentence, string search)
+        {
+
+            if (search.Equals(sentence) || search.Equals(sentence.ToLower()))
+            {
+                return true;
+            }
+
+            string[] SentenceArr = sentence.Split();
+
+            foreach (string word in SentenceArr)
+            {
+                if (search.Equals(word) || search.Equals(word.ToLower()))
+                {
+                    return true;
+                }
+                else if (word.EndsWith(search) || word.StartsWith(search))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }   
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            string search = Console.ReadLine();
+
+            Console.WriteLine(str.CustomContain(search));
+
+        }
+    }
+}
